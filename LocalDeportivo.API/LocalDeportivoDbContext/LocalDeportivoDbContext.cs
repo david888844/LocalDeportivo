e3e35cd652c62225a30f2689aee30d4b56328a3e
david888844 <dsalzate93883@umanizales.edu.co>
2022-04-28 12:08:52 -0500

    Create DbContext

---
+++ LocalDeportivo.API/LocalDeportivoDbContext/LocalDeportivoDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalDeportivo.API.LocalDeportivoDbContext
+{
+    public class LocalDeportivoDbContext
+    {
+        
+    }
+}
